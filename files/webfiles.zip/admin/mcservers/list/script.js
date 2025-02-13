c4d6fa62038b9ce69e2e0129e5261c8f4f3123a8
Tom <tomgriffiths2007@gmail.com>
2025-02-13 14:55:54 +0000

    Made server status asynchronous

--- files/webfiles.zip/admin/mcservers/list/script.js
+++ files/webfiles.zip/admin/mcservers/list/script.js
@@ -1,40 +1,45 @@
 function serverState(serverId){
-    const serverData = JSON.parse(ajax_mcserver('/admin/mcservers/api/?function=serverStats&id=' + serverId));
-    const statebutton = document.getElementById('server' + serverId + 'statebutton');
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function(){
+        const serverData = JSON.parse(this.responseText);
+        const statebutton = document.getElementById('server' + serverId + 'statebutton');
 
-    if(serverData['state'] === "stopped"){
-        statebutton.style.backgroundColor = "red";
-        statebutton.innerHTML = "Start Server";
+        if(serverData['state'] === "stopped"){
+            statebutton.style.backgroundColor = "red";
+            statebutton.innerHTML = "Start Server";
+        }
+        else if(serverData['state'] === "starting"){
+            statebutton.style.backgroundColor = "orange";
+            statebutton.innerHTML = "Starting...";
+            setTimeout(() => {
+                serverState(serverId);
+            }, 2000);
+        }
+        else if(serverData['state'] === "online"){
+            statebutton.style.backgroundColor = "lime";
+            statebutton.innerHTML = "Stop Server";
+        }
+        else if(serverData['state'] === "stopping"){
+            statebutton.style.backgroundColor = "orange";
+            statebutton.innerHTML = "Stopping...";
+            setTimeout(() => {
+                serverState(serverId);
+            }, 2000);
+        }
+        else if(serverData['state'] === "backup"){
+            statebutton.style.backgroundColor = "blue";
+            statebutton.innerHTML = "Backing up";
+            setTimeout(() => {
+                serverState(serverId);
+            }, 2000);
+        }
+        else{
+            statebutton.style.backgroundColor = "lightgrey";
+            statebutton.innerHTML = "Unknown";
+        }
     }
-    else if(serverData['state'] === "starting"){
-        statebutton.style.backgroundColor = "orange";
-        statebutton.innerHTML = "Starting...";
-        setTimeout(() => {
-            serverState(serverId);
-        }, 2000);
-    }
-    else if(serverData['state'] === "online"){
-        statebutton.style.backgroundColor = "lime";
-        statebutton.innerHTML = "Stop Server";
-    }
-    else if(serverData['state'] === "stopping"){
-        statebutton.style.backgroundColor = "orange";
-        statebutton.innerHTML = "Stopping...";
-        setTimeout(() => {
-            serverState(serverId);
-        }, 2000);
-    }
-    else if(serverData['state'] === "backup"){
-        statebutton.style.backgroundColor = "blue";
-        statebutton.innerHTML = "Backing up";
-        setTimeout(() => {
-            serverState(serverId);
-        }, 2000);
-    }
-    else{
-        statebutton.style.backgroundColor = "lightgrey";
-        statebutton.innerHTML = "Unknown";
-    }
+    xhttp.open("GET", '/admin/mcservers/api/?function=serverStats&id=' + serverId);
+    xhttp.send();
 }
 function changeState(serverId){
     const statebutton = document.getElementById('server' + serverId + 'statebutton');
@@ -47,9 +52,6 @@ function changeState(serverId){
         statebutton.innerHTML = "Loading...";
         ajax_mcserver('/admin/mcservers/api/?function=start_server&id=' + serverId);
         serverState(serverId);
-    }
-    else{
-        
     }
     statebutton.click();
 }
